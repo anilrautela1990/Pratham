37774c31d28c558dd054ec89972eb4c1db42be33
Anil Rautela <anil.rautela1990@gmail.com>
2016-12-17 09:37:07 +0530

    ashish changes merged.

--- js/prathamControllersClient.js
+++ js/prathamControllersClient.js
@@ -96,7 +96,6 @@ app.controller("mainCtrl", function($scope,$http,$cookieStore,$state,$window) {
             $window.location.href = '/';
         }
     })();
-    
     $scope.logout = function(){
        $cookieStore.remove('user_id');
        $cookieStore.remove('comp_guid');
@@ -110,6 +109,7 @@ app.controller("dashboard", function($scope,$http,$cookieStore) {
 
 app.controller("leads", function($scope,$http,$cookieStore,$uibModal) {
 	 ($scope.getLeads = function(){
+		 angular.element(".loader").show();
          $http({
             method: "POST",
                 url: "http://120.138.8.150/pratham/User/UserDtls/ByUserType",
@@ -120,14 +120,20 @@ app.controller("leads", function($scope,$http,$cookieStore,$uibModal) {
 				}
             }).success(function(data) {
 				console.log(data);
+			 	angular.element(".loader").hide();
              $scope.leads = data;
-			}).error(function() {});
+			}).error(function() {
+			 angular.element(".loader").hide();
+		 });
      })();
     
     $scope.leadDetail = function(selectedItem){
         var modalInstance = $uibModal.open({
             templateUrl: 'leadDetail.html',
             controller: 'leadDetail',
+			size:'lg',
+			backdrop:'static',
+			keyboard:false,
             resolve: {
               item: function () {
                 return $scope.leads[selectedItem];;
@@ -170,16 +176,19 @@ app.controller("addLead", function($scope,$http,$state,$cookieStore) {
 				}
             }).success(function(data) {
 				console.log(data);
-                alert(data.user_id);
-                $cookieStore.put('user_id',data.user_id);
+                $cookieStore.put('lead_id',data.user_id);
                 $state.go("/ProjectDetails");
 			}).error(function() {});
 		}
 	}
 });
-app.controller("projectDetails", function($scope,$http,$state,$cookieStore) {
+app.controller("projectDetails", function($scope,$http,$state,$cookieStore,$compile) {
+	if($cookieStore.get('lead_id') == undefined){
+		$state.go('/AddLead');
+	}
 	$scope.flatStatus = ['vacant','userinterest','mgmtquota','blockedbyadvnc','blockedbynotadvnc','sold'];
     ($scope.getProjectList = function(){
+			angular.element(".loader").show();
             $http({
                 method: "POST",
                 url: "http://120.138.8.150/pratham/Proj/ProjDtls/ByCompGuid",
@@ -190,10 +199,14 @@ app.controller("projectDetails", function($scope,$http,$state,$cookieStore) {
             }).success(function(data) {
 				console.log(data);
 				$scope.projectList = data;
-			}).error(function() {});
+				angular.element(".loader").hide();
+			}).error(function() {
+				angular.element(".loader").hide();
+			});
     })();
 	
 	$scope.getPhaseList = function(projectName){
+		angular.element(".loader").show();
 			$http({
 				method: "POST",
                 url: "http://120.138.8.150/pratham/Proj/PhaseDtls/ByPhaseProjId",
@@ -205,9 +218,13 @@ app.controller("projectDetails", function($scope,$http,$state,$cookieStore) {
             }).success(function(data) {
 				console.log(data);
 				$scope.phaseList = data;
-			}).error(function() {});
+				angular.element(".loader").hide();
+			}).error(function() {
+				angular.element(".loader").hide();
+			});
 	};
 	$scope.getBlockList = function(phase,projectName){
+		angular.element(".loader").show();
 			$http({
 				method: "POST",
                 url: "http://120.138.8.150/pratham/Proj/BlockDtls/ByPhaseBlocksId",
@@ -219,9 +236,13 @@ app.controller("projectDetails", function($scope,$http,$state,$cookieStore) {
             }).success(function(data) {
 				console.log(data);
 				$scope.blockList = data;
-			}).error(function() {});
+				angular.element(".loader").hide();
+			}).error(function() {
+				angular.element(".loader").hide();
+			});
 	};
 	$scope.getUnits = function(blocks){
+		angular.element(".loader").show();
 			$http({
 				method: "POST",
                 url: "http://120.138.8.150/pratham/Proj/UnitDtls/ByUnitDtlsBlocksId",
@@ -233,11 +254,71 @@ app.controller("projectDetails", function($scope,$http,$state,$cookieStore) {
             }).success(function(data) {
 				console.log(data);
 				$scope.units = data;
-			}).error(function() {});
+				angular.element(".loader").hide();
+			}).error(function() {
+				angular.element(".loader").hide();
+			});
 	};
 	$scope.selectUnit = function(unitId,projectDetails){
-//		$scope.unitDetail =  $scope.units[unitId-1];
-		var projectRow = '<tr><td>'+$scope.units[unitId-1].UnitDtls_Id+'</td><td>'+$scope.units[unitId-1].UnitDtls_Type+'</td><td>Phase</td><td>'+$scope.units[unitId-1].UnitDtls_Type+'</td><td>Block '+$scope.units[unitId-1].UnitDtls_Block_Id+'</td><td>'+$scope.units[unitId-1].UnitDtls_BRoom+'BHK - '+$scope.units[unitId-1].UnitDtls_No+' - '+$scope.units[unitId-1].UnitDtls_Floor+' Floor</td></tr>';
-		$("#projectList > tbody").append(projectRow);
+		if($scope.units[unitId-1].UnitDtls_Status == 1){
+			if($("#unit"+$scope.units[unitId-1].UnitDtls_Id).hasClass('selected')){
+				$("tr#"+$scope.units[unitId-1].UnitDtls_Id).remove();
+			}
+			else{
+				var projectRow = '<tr id="'+$scope.units[unitId-1].UnitDtls_Id+'"><td class="unitId">'+$scope.units[unitId-1].UnitDtls_Id+'</td><td>'+$scope.units[unitId-1].UnitDtls_Type+'</td><td>Phase</td><td>'+$scope.units[unitId-1].UnitDtls_Type+'</td><td>Block '+$scope.units[unitId-1].UnitDtls_Block_Id+'</td><td>'+$scope.units[unitId-1].UnitDtls_BRoom+'BHK - '+$scope.units[unitId-1].UnitDtls_No+' - '+$scope.units[unitId-1].UnitDtls_Floor+' Floor</td><td><span class="glyphicon glyphicon-trash delete" ng-click="deleteRow('+$scope.units[unitId-1].UnitDtls_Id+')"></span></td></tr>';
+				
+				var projectRowComplied = $compile(projectRow)($scope);
+				angular.element(document.getElementById('projectList')).append(projectRowComplied);
+			}
+			$("#unit"+$scope.units[unitId-1].UnitDtls_Id).toggleClass('selected');
+		}
+		else{
+			alert($scope.flatStatus[$scope.units[unitId-1].UnitDtls_Status-1]);
+		}
+	};
+	$scope.deleteRow = function(rowId){
+		$("tr#"+rowId).remove();
+		$("#unit"+rowId).removeClass('selected');
+	};
+	$scope.saveLead = function(projectObj){
+		var unitIds = [];
+		$(".unitId").each(function(index){
+			console.log( index + ": " + $( this ).text() );
+			unitIds.push($( this ).text());
+		});
+		angular.element(".loader").show();
+			$http({
+				method: "POST",
+                url: "http://120.138.8.150/pratham/User/SaveUser",
+                ContentType: 'application/json',
+                data: {
+				  "user_id":$cookieStore.get('lead_id'),
+				  "user_proj":{
+					  "UserProj_comp_guid":$cookieStore.get('comp_guid'),
+					  "UserProj_projid":parseInt(projectObj.projectName),
+					  "UserProj_user_id":$cookieStore.get('lead_id'),
+					  "projDlts":{
+						  "ProjId": parseInt(projectObj.projectName)
+					  },
+                      "phaseDtls":{
+                          "Phase_Id": parseInt(projectObj.phase)
+                      },
+                      "blockDtls":{
+                          "Blocks_Id": parseInt(projectObj.blocks)
+                      },
+                      "unitdtls":{
+                            "UnitDtls_Id":parseInt(unitIds[0])
+					  }
+					}
+				}
+            }).success(function(data) {
+				if(data.user_id!=null){
+					$cookieStore.remove('lead_id');
+					$state.go('/Leads');
+					angular.element(".loader").hide();
+				}
+			}).error(function() {
+				angular.element(".loader").hide();
+			});
 	};
 });
