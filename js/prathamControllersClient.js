968ff1ded9a68b100d2942337a68dc4fdfdecb87
Ashish Agrawal <ashishagrawal89@gmail.com>
2016-12-30 00:44:12 +0530

    Show lead projects while adding projects.

--- js/prathamControllersClient.js
+++ js/prathamControllersClient.js
@@ -26,7 +26,7 @@ app.controller("firmRegister", function($scope, $http, $state, $cookieStore) {
                     "comp_websiteurl": formObj.firmWebsiteURL
                 }
             }).success(function(data) {
-                console.log(data);
+                //console.log(data);
                 alert("Your Company Data Saved");
                 $cookieStore.put('comp_guid', data.comp_guid);
                 $state.go('/AdminCreation');
@@ -54,7 +54,7 @@ app.controller("adminCreation", function($scope, $http, $state, $cookieStore) {
                     "user_password": formObj.password
                 }
             }).success(function(data) {
-                console.log(data);
+                //console.log(data);
                 alert("Admin User Created");
                 $state.go('/Login');
             }).error(function() {});
@@ -76,7 +76,7 @@ app.controller("login", function($scope, $http, $cookieStore, $window) {
                     "user_password": formObj.password
                 }
             }).success(function(data) {
-                console.log(data);
+                //console.log(data);
                 if (data.user_id == 0) {
                     alert("User Does Not Exist!");
                 } else {
@@ -117,7 +117,7 @@ app.controller("dashboard", function($scope, $http, $cookieStore) {
 });
 
 app.controller("leads", function($scope, $http, $cookieStore, $uibModal, $state) {
-    $scope.searchLead   = '';     // set the default search/filter term
+    $scope.searchLead = ''; // set the default search/filter term
     ($scope.getLeads = function() {
         angular.element(".loader").show();
         $http({
@@ -129,7 +129,7 @@ app.controller("leads", function($scope, $http, $cookieStore, $uibModal, $state)
                 "user_type": 3
             }
         }).success(function(data) {
-            console.log(data);
+            //console.log(data);
             angular.element(".loader").hide();
             $scope.leads = data;
         }).error(function() {
@@ -156,8 +156,8 @@ app.controller("leads", function($scope, $http, $cookieStore, $uibModal, $state)
     };
 });
 app.controller("leadDetail", function($scope, $uibModalInstance, $state, item) {
-	$scope.states = ["Delhi"];
-	$scope.cities = ["New Delhi"];
+    $scope.states = ["Delhi"];
+    $scope.cities = ["New Delhi"];
     $scope.lead = item;
     if ($scope.lead.projectlst != null) {
         $scope.leadProjects = [];
@@ -179,25 +179,27 @@ app.controller("leadDetail", function($scope, $uibModalInstance, $state, item) {
             }
 
         }
-        console.log(JSON.stringify($scope.leadProjects));
+        //console.log(JSON.stringify($scope.leadProjects));
     }
     $scope.ok = function() {
         $uibModalInstance.close();
     };
-    $scope.deleteRow = function(rowId){
-      angular.element("tr#"+rowId).remove();
+    $scope.deleteRow = function(rowId) {
+        angular.element("tr#" + rowId).remove();
     };
-    $scope.addLeadProjects = function(leadId){
+    $scope.addLeadProjects = function(leadId) {
         $uibModalInstance.close();
-        $state.go("/ProjectDetails",{"leadID": leadId});
+        $state.go("/ProjectDetails", {
+            "leadID": leadId
+        });
     };
-        
+
 
 });
 
 app.controller("addLead", function($scope, $http, $state, $cookieStore) {
     $scope.pageTitle = "Add Lead";
-	$scope.addLeadBtn = true;
+    $scope.addLeadBtn = true;
     $scope.addLead = function(formObj, formName) {
         $scope.submit = true;
         if ($scope[formName].$valid) {
@@ -223,10 +225,12 @@ app.controller("addLead", function($scope, $http, $state, $cookieStore) {
                     "user_gender": parseInt(formObj.gender),
                 }
             }).success(function(data) {
-                console.log(data);
+                //console.log(data);
                 if (data.user_id != 0) {
-//                    $cookieStore.put('lead_id', data.user_id);
-                    $state.go("/ProjectDetails",{"leadID": data.user_id});
+                    //                    $cookieStore.put('lead_id', data.user_id);
+                    $state.go("/ProjectDetails", {
+                        "leadID": data.user_id
+                    });
                 } else {
                     alert("Some Error!");
                 }
@@ -235,58 +239,59 @@ app.controller("addLead", function($scope, $http, $state, $cookieStore) {
     };
 });
 
-app.controller("editLead", function($scope, $http, $state, $cookieStore, $stateParams,$filter) {
-$scope.pageTitle = "Edit Lead";
-$scope.editLeadBtn = true;
-    
-($scope.getLeadDetail = function(){
-		$scope.leadId = $stateParams.leadID;
-		$http({
-                method: "POST",
-                url: "http://120.138.8.150/pratham/User/UserDtls",
-                ContentType: 'application/json',
-                data: {
-					"user_id": $scope.leadId,
-                    "user_comp_guid": $cookieStore.get('comp_guid')
-                }
-            }).success(function(data) {
-                console.log(data);
-			var state = data.user_state;
-			var city = data.user_city;
+app.controller("editLead", function($scope, $http, $state, $cookieStore, $stateParams, $filter) {
+    $scope.pageTitle = "Edit Lead";
+    $scope.editLeadBtn = true;
+    ($scope.getLeadDetail = function() {
+        angular.element(".loader").show();
+        $scope.leadId = $stateParams.leadID;
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/User/UserDtls",
+            ContentType: 'application/json',
+            data: {
+                "user_id": $scope.leadId,
+                "user_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+            //console.log(data);
+            var state = data.user_state;
+            var city = data.user_city;
             var dob = $filter('date')(data.user_dob, 'MMM dd, yyyy');
-            
-			if(state == 0){
-				state = "";
-			}
-			if(city == 0){
-				city = "";
-			}
-            if(dob == "Jan 01, 0001"){
+
+            if (state == 0) {
+                state = "";
+            }
+            if (city == 0) {
+                city = "";
+            }
+            if (dob == "Jan 01, 0001") {
                 dob = "";
             }
-			if(data.user_id != 0){
-				$scope.addLead = {
-					firstName: data.user_first_name,
-					middleName: data.user_middle_name,
-					lastName: data.user_last_name,
-					mobileNumber: data.user_mobile_no,
-					emailId: data.user_email_address,
-					dob: dob,
-					gender: data.user_gender,
-					country: data.user_country,
-					state: state+"",
-					city: city+"",
-					address: data.user_address,
-					zip: data.user_zipcode
-				}
-			}else{
-				$state.go("/Leads");
-			}
-            }).error(function() {});
-	})();
-    
-     $scope.updateLead = function(formObj, formName) {
-         console.log(formObj);
+            if (data.user_id != 0) {
+                $scope.addLead = {
+                    firstName: data.user_first_name,
+                    middleName: data.user_middle_name,
+                    lastName: data.user_last_name,
+                    mobileNumber: data.user_mobile_no,
+                    emailId: data.user_email_address,
+                    dob: dob,
+                    gender: data.user_gender,
+                    country: data.user_country,
+                    state: state + "",
+                    city: city + "",
+                    address: data.user_address,
+                    zip: data.user_zipcode
+                }
+                angular.element(".loader").hide();
+            } else {
+                $state.go("/Leads");
+            }
+        }).error(function() {});
+    })();
+
+    $scope.updateLead = function(formObj, formName) {
+        //console.log(formObj);
         $scope.submit = true;
         if ($scope[formName].$valid) {
             $http({
@@ -311,7 +316,7 @@ $scope.editLeadBtn = true;
                     "user_gender": parseInt(formObj.gender),
                 }
             }).success(function(data) {
-                console.log(data);
+                //console.log(data);
                 if (data.user_id != 0) {
                     //$cookieStore.put('lead_id', data.user_id);
                     $state.go("/Leads");
@@ -324,9 +329,51 @@ $scope.editLeadBtn = true;
 });
 app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $compile, $stateParams) {
     $scope.leadId = $stateParams.leadID;
-    if ($scope.leadId == undefined) {        
+    if ($scope.leadId == undefined) {
         $state.go('/AddLead');
     }
+    ($scope.getLeadProjects = function() {
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/User/UserDtls",
+            ContentType: 'application/json',
+            data: {
+                "user_id": $scope.leadId,
+                "user_comp_guid": $cookieStore.get('comp_guid')
+            }
+        }).success(function(data) {
+//			//console.log(data);
+            if (data.user_id != 0) {
+                if (data.projectlst != null) {
+                    $scope.leadProjects = [];
+                    for (i = 0; i < data.projectlst.length; i++) {
+                        for (j = 0; j < data.projectlst[i].Lstphases.length; j++) {
+                            for (k = 0; k < data.projectlst[i].Lstphases[j].LstofBlocks.length; k++) {
+                                for (l = 0; l < data.projectlst[i].Lstphases[j].LstofBlocks[k].Lstofunitdtls.length; l++) {
+									
+									$scope.leadUnitObj = {};
+									$scope.leadUnitObj.ProjId = data.projectlst[i].ProjId;
+									$scope.leadUnitObj.Phase_Id = data.projectlst[i].Lstphases[j].Phase_Id;
+									$scope.leadUnitObj.Blocks_Id = data.projectlst[i].Lstphases[j].LstofBlocks[k].Blocks_Id;
+									$scope.leadUnitObj.UnitDtls = data.projectlst[i].Lstphases[j].LstofBlocks[k].Lstofunitdtls[k];									
+									$scope.leadProjects.push($scope.leadUnitObj);
+                                }
+
+                            }
+
+                        }
+
+                    }
+                    //console.log(JSON.stringify($scope.leadProjects));
+                }
+                angular.element(".loader").hide();
+            } else {
+                $state.go("/Leads");
+            }
+        }).error(function() {});
+    })();
+
     $scope.flatStatus = ['vacant', 'userinterest', 'mgmtquota', 'blockedbyadvnc', 'blockedbynotadvnc', 'sold'];
     $scope.flatStatusText = ['Vacant', 'User Interested', 'Management Quota', 'Blocked By Paying Advance', 'Blocked By Not Paying Advance', 'Sold'];
     ($scope.getProjectList = function() {
@@ -340,7 +387,6 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                 "Proj_comp_guid": $cookieStore.get('comp_guid')
             }
         }).success(function(data) {
-            console.log(data);
             $scope.projectList = data;
             angular.element(".loader").hide();
         }).error(function() {
@@ -364,7 +410,7 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                 "Phase_comp_guid": $cookieStore.get('comp_guid')
             }
         }).success(function(data) {
-            console.log(data);
+            //console.log(data);
             $scope.phaseList = data;
             angular.element(".loader").hide();
         }).error(function() {
@@ -389,7 +435,7 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                 "Blocks_Phase_Id": phase
             }
         }).success(function(data) {
-            console.log(data);
+            //console.log(data);
             $scope.blockList = data;
             angular.element(".loader").hide();
         }).error(function() {
@@ -397,8 +443,8 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
         });
     };
     $scope.getUnits = function(blocks) {
-        for(i=0;i<$scope.blockList.length;i++){
-            if($scope.blockList[i].Blocks_Id == blocks){
+        for (i = 0; i < $scope.blockList.length; i++) {
+            if ($scope.blockList[i].Blocks_Id == blocks) {
                 $scope.blockFloors = $scope.blockList[i].Blocks_Floors;
                 $scope.blockFloorUnits = $scope.blockList[i].Blocks_UnitPerfloor;
             }
@@ -414,40 +460,40 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                 "UnitDtls_comp_guid": $cookieStore.get('comp_guid')
             }
         }).success(function(data) {
-//            console.log(JSON.stringify(data));
+            //            //console.log(JSON.stringify(data));
             $scope.selectedUnits = [];
             $(".dispNone").each(function(index) {
                 var projObj = $(this).text();
                 projObj = angular.fromJson(projObj);
                 $scope.selectedUnits.push(projObj.UnitDtls_Id);
             });
-            
-            for(i=0;i<data.length;i++){
-                for(j=0;j<$scope.selectedUnits.length;j++){
-                    if(data[i].UnitDtls_Id == $scope.selectedUnits[j]){
+
+            for (i = 0; i < data.length; i++) {
+                for (j = 0; j < $scope.selectedUnits.length; j++) {
+                    if (data[i].UnitDtls_Id == $scope.selectedUnits[j]) {
                         data[i].markUp = "selected";
                         break;
                     }
-                }    
+                }
             }
             $scope.perFloorUnits = [];
             var count = 0;
-            for(k=0;k<$scope.blockFloors;k++){
+            for (k = 0; k < $scope.blockFloors; k++) {
                 var floorUnits = [];
-                for(l=0;l<$scope.blockFloorUnits;l++){
+                for (l = 0; l < $scope.blockFloorUnits; l++) {
                     floorUnits.push(data[count]);
                     count++;
-                }                
+                }
                 $scope.perFloorUnits.push(floorUnits);
             }
             $scope.units = data;
             angular.element(".loader").hide();
-            
+
         }).error(function() {
             angular.element(".loader").hide();
         });
     };
-    $scope.markUnits = function(){
+    $scope.markUnits = function() {
         alert($scope.selectedUnits);
     };
     $scope.selectUnit = function(unitId, projectDetails) {
@@ -458,12 +504,12 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                         $("tr#" + $scope.units[i].UnitDtls_Id).remove();
                     } else {
                         var projObj = {};
-                        projObj.ProjId = $scope.projectDetails.projectName;
-                        projObj.Phase_Id = $scope.projectDetails.phase;
-                        projObj.Blocks_Id = $scope.projectDetails.blocks;
+                        projObj.ProjId = parseInt($scope.projectDetails.projectName);
+                        projObj.Phase_Id = parseInt($scope.projectDetails.phase);
+                        projObj.Blocks_Id = parseInt($scope.projectDetails.blocks);
                         projObj.UnitDtls_Id = $scope.units[i].UnitDtls_Id;
                         projObj = JSON.stringify(projObj);
-                        console.log(projObj);
+                        //console.log(projObj);
 
                         var projectRow = '<tr id="' + $scope.units[i].UnitDtls_Id + '"><td><div class="dispNone">' + projObj + '</div>' + $scope.units[i].UnitDtls_BRoom + 'BHK - ' + $scope.units[i].UnitDtls_No + ' - ' + $scope.units[i].UnitDtls_Floor + ' Floor</td><td>' + $scope.units[i].UnitDtls_Msrmnt + ' sq ft</td><td><span class="glyphicon glyphicon-trash delete" ng-click="deleteRow(' + $scope.units[i].UnitDtls_Id + ')"></span></td></tr>';
                         var projectRowComplied = $compile(projectRow)($scope);
@@ -483,12 +529,12 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
     $scope.saveLead = function(projectObj) {
         var projJson = [];
         $(".dispNone").each(function(index) {
-            console.log(index + ": " + $(this).text());
+            //console.log(index + ": " + $(this).text());
             var projObj = $(this).text();
             projObj = angular.fromJson(projObj);
             projJson.push(projObj);
         });
-        console.log(projJson);
+        //console.log(projJson);
         angular.element(".loader").show();
         $http({
             method: "POST",
@@ -508,7 +554,7 @@ app.controller("projectDetails", function($scope, $http, $state, $cookieStore, $
                 $state.go('/Leads');
                 angular.element(".loader").hide();
             }
-            console.log(JSON.stringify(data));
+            //console.log(JSON.stringify(data));
         }).error(function() {
             angular.element(".loader").hide();
         });
@@ -535,8 +581,8 @@ app.controller("convertCustomer", function($scope, $http, $compile) {
     };
     $scope.addCustomer = function(formObj, formName) {
         var formData = JSON.stringify(formObj);
-        console.log(formData);
-        console.log(Object.keys(formObj).length);
+        //console.log(formData);
+        //console.log(Object.keys(formObj).length);
     };
     $scope.appendFields = function() {
         angular.element("#children").html('');
