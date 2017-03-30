d6bcac747969d2654586e4efe6e425f02932c225
Ashish Agrawal <ashishagrawal89@gmail.com>
2017-03-30 19:38:26 +0530

    Datepicker - Partial.

--- js/prathamControllersClient.js
+++ js/prathamControllersClient.js
@@ -5283,6 +5283,7 @@ app.controller("prospects", function($scope, $http, $cookieStore, $uibModal, $st
 });
 
 app.controller("prospectDetail", function($scope, $uibModalInstance, $state, $cookieStore, $http, myService, item) {
+    $scope.timeslots = ['10:00 AM','10:30 AM','11:00 AM','11:30 AM','12:00 PM','12:30 PM','01:00 PM','01:30 PM','02:00 PM','02:30 PM','03:00 PM','03:30 PM','04:00 PM','04:30 PM','05:00 PM','05:30 PM','06:00 PM','07:00 PM','07:30 PM','08:30 PM'];
     $scope.leadType = ['hot', 'warm', 'cold'];
     $scope.states = ["Delhi"];
     $scope.cities = ["New Delhi"];
@@ -5381,7 +5382,7 @@ app.controller("prospectDetail", function($scope, $uibModalInstance, $state, $co
                     "sitevisit_blockid": formObj.blocks,
                     "sitevisit_required": formObj.required,
                     "sitevisit_walkin": formObj.walkIn,
-                    "sitevisit_pickupdatetime": "2017-08-02",
+                    "sitevisit_pickupdatetime": formObj.datetime+' T '+formObj.time,
                     "sitevisit_contactperson_name": formObj.personName,
                     "sitevisit_contactperson_mobile": formObj.personMobile,
                     "sitevisit_pickupaddress": formObj.personAddress,
