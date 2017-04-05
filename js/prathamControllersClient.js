9abcba2260369495eba2990d2ef25a5246258328
Ashish Agrawal <ashishagrawal89@gmail.com>
2017-04-05 20:30:56 +0530

    Email template creation - partial.

--- js/prathamControllersClient.js
+++ js/prathamControllersClient.js
@@ -1,4 +1,4 @@
-app.controller("home", function($scope, $http) {
+app.controller("home", function($scope, $http, $rootScope) {
     $scope.title = "Pratham - Pre Login";
     $scope.clientCss = "prathamClient";
 });
@@ -101,7 +101,9 @@ app.controller("login", function($scope, $http, $cookieStore, $window) {
     };
 });
 /* After Login*/
-app.controller("mainCtrl", function($scope, $http, $cookieStore, $state, $window) {
+app.controller("mainCtrl", function($scope, $rootScope, $http, $cookieStore, $state, $window) {    
+    $rootScope.appMsg = "";
+    $rootScope.showAppMsg = false;
     $scope.title = "Pratham :: Home";
     $scope.clientCss = "prathamClient";
     ($scope.checkLogin = function() {
@@ -5647,10 +5649,62 @@ app.controller("emailTemplatesCtrl", function($scope, $http, $state, $cookieStor
     $scope.pageTitle = "Email Templates";
 });
 
-app.controller("createNewEmailTemplateCtrl", function($scope, $http, $state, $cookieStore) {
+app.controller("createNewEmailTemplateCtrl", function($scope, $rootScope, $http, $state, $cookieStore) {
     $scope.pageTitle = "Create New Email Template";
+    $scope.template = {
+        mergeFieldType:"",
+        fields:""        
+    };
     ($scope.loadEditor = function(){
             $('#contentEditor').summernote();
     })();
+    
+    $scope.getMergedFieldTypes = (function(){
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Comp/ModulesGetAlrt",
+            ContentType: 'application/json',
+            data: {
+                "module_id": 0
+            }
+        }).success(function(data) {
+            $scope.mergeFieldTypes = data;
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    })();
+    
+    $scope.getFields = function(moduleId){
+        angular.element(".loader").show();
+        $http({
+            method: "POST",
+            url: "http://120.138.8.150/pratham/Comp/SubModulesGet",
+            ContentType: 'application/json',
+            data: {
+                "module_id": moduleId
+            }
+        }).success(function(data) {
+            if(data[0].modfield_ErrorDesc == "-1 | Module fields do not exist for this Module"){
+                $rootScope.appMsg = "Module fields do not exist for this Module";
+                $rootScope.showAppMsg = true;
+                $scope.fields = [];
+                $scope.template.fields = "";
+                $scope.template.copyMergedFields = "";
+            }
+            else{
+                $rootScope.appMsg = "";
+                $rootScope.showAppMsg = false;
+                $scope.fields = data;   
+            }
+            angular.element(".loader").hide();
+        }).error(function() {
+            angular.element(".loader").hide();
+        });
+    }
+    
+    $scope.copyMergedFields = function(field){
+        $scope.template.copyMergedFields = field;
+    }
 });
-
